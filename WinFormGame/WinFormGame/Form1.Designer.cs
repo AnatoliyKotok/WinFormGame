013f26e7f315f44cab5bf0604d5276209a385f43
AnatoliyKotok <61416716+AnatoliyKotok@users.noreply.github.com>
2020-11-17 12:30:58 +0200

    Add files via upload

---
+++ WinFormGame/WinFormGame/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WinFormGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pctB = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pctB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctB
+            // 
+            this.pctB.BackColor = System.Drawing.Color.Maroon;
+            this.pctB.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pctB.Location = new System.Drawing.Point(0, 80);
+            this.pctB.Name = "pctB";
+            this.pctB.Size = new System.Drawing.Size(40, 40);
+            this.pctB.TabIndex = 0;
+            this.pctB.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("Ravie", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(-5, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(802, 314);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pctB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctB;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinFormGame/WinFormGame/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormGame
+{
+    public partial class Form1 : Form
+    {
+        private int dirX, dirY;
+        private PictureBox fruit;
+        private int _width =700;
+        private int _height =700;
+        private int f1, f2;
+        private int sideOfSize = 40;
+        private int score=0;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Height = _height;
+            this.Width = _width;
+            fruit = new PictureBox();
+            fruit.BackColor = Color.Yellow;
+            fruit.Size = new Size(sideOfSize, sideOfSize);
+            timer1.Tick += new EventHandler(update);
+            timer1.Interval = 250;
+            timer1.Start();
+            generateMap();
+            GenerateFruit();
+            this.KeyDown += new KeyEventHandler(OKP);
+            pctB.Size = new Size(sideOfSize, sideOfSize);
+            label1.Width = this.Width;
+        }
+        private void generateMap()
+        {
+            for (int i = 0; i <_width/sideOfSize; i++)
+            {
+                PictureBox pic = new PictureBox();
+                pic.BackColor = Color.Black;
+                pic.Location = new Point(0, sideOfSize * i);
+                pic.Size = new Size(_width - 100,1);
+                this.Controls.Add(pic);
+                pic.Visible = false;
+            }
+            for (int i = 0; i <= _height / sideOfSize; i++)
+            {
+                PictureBox pic = new PictureBox();
+                pic.BackColor = Color.Black;
+                pic.Location = new Point( sideOfSize * i,0);
+                pic.Size = new Size(1,_width - 100);
+                this.Controls.Add(pic);
+                pic.Visible = false;
+            }
+        }
+        private void GenerateFruit()
+        {
+            Random r = new Random();
+            f1 = r.Next(0, _height-sideOfSize);
+            int temp = f1 % sideOfSize;
+            f1 -= temp;
+            f2 = r.Next(0, _height- sideOfSize);
+            int tmp2 = f2 % sideOfSize;
+            f2 -= tmp2;
+           
+            fruit.Location = new Point(f1, f2);
+            
+            this.Controls.Add(fruit);
+
+        } 
+        private void chackBorders()
+        {
+            if (pctB.Location.X <0 )
+            {
+                dirX = 1;
+            }
+            else if (pctB.Location.X > _height-90)
+            {
+                dirX = -1;
+            }
+            if (pctB.Location.Y < label1.Height)
+            {
+                dirY = 1;
+            }
+            else if (pctB.Location.Y > _height-100)
+            {
+                dirY = -1;
+            }
+            
+        }
+        private void catchFruit()
+        {
+            if (pctB.Location.X == f1 && pctB.Location.Y == f2)
+            {
+                label1.Text = "Score:"+ ++score;
+                GenerateFruit();
+            } 
+        }
+
+        
+
+        private void update(object sender,EventArgs e)
+        {
+            chackBorders();
+            catchFruit();
+            pctB.Location = new Point(pctB.Location.X + dirX * sideOfSize, pctB.Location.Y + dirY * sideOfSize);
+        }
+        private void OKP(object sender,KeyEventArgs e)
+        {
+            
+            switch (e.KeyCode.ToString())
+            {
+                case "Right":
+                    dirX = 1;
+                    dirY = 0;
+                    break;
+                case "Left":
+                    dirX = -1;
+                    dirY = 0;
+                    break;
+                case "Up":
+                    dirY = -1;
+                    dirX = 0;
+                    break;
+                case "Down":
+                    dirY = 1;
+                    dirX = 0;
+                    break;
+            }
+        }
+        
+    }
+}
